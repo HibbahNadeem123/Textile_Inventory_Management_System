513364e66de3eb1abe53141ac293f38fa482739f
Rukhma <tassadaq187@gmail.com>
2020-06-25 18:23:17 +0500

    State Design Pattern + Sign up for customer added

---
+++ ProjectSDA/ProjectSDA/Design Patterns/StatePattern.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSDA.Design_Patterns
+{
+    public class StatePattern
+    {
+        static void Main()
+        {
+            // Setup ProductDp in a state
+
+            ProductDp c = new ProductDp(new AvailableStock(), 3);
+
+            // Issue requests, which toggles state
+
+            c.Request();
+            c.Request();
+            c.Request();
+            c.Request();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+
+    /// The 'State' abstract class
+
+    /// </summary>
+
+    abstract class State
+
+    {
+        public abstract void Handle(ProductDp ProductDp);
+    }
+
+    /// <summary>
+
+    /// A 'ConcreteState' class
+
+    /// </summary>
+
+    class AvailableStock : State
+
+    {
+        public override void Handle(ProductDp ProductDp)
+        {
+            if (ProductDp.qty <= 0)
+            {
+                ProductDp.State = new NotInStock();
+            }
+
+        }
+    }
+
+    /// <summary>
+
+    /// A 'ConcreteState' class
+
+    /// </summary>
+
+    class NotInStock : State
+
+    {
+        public override void Handle(ProductDp ProductDp)
+        {
+            if (ProductDp.qty <= 0)
+            {
+                ProductDp.State = new AvailableStock();
+            }
+        }
+    }
+
+    /// <summary>
+
+    /// The 'ProductDp' class
+
+    /// </summary>
+
+    class ProductDp
+    {
+        private State _state;
+        public int qty;
+        // Constructor
+
+        public ProductDp(State state, int q)
+        {
+            this.State = state;
+            qty = q;
+        }
+
+        // Gets or sets the state
+
+        public State State
+        {
+            get { return _state; }
+            set
+
+            {
+                _state = value;
+                Console.WriteLine("State: " +
+                  _state.GetType().Name);
+            }
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+    }
+}
