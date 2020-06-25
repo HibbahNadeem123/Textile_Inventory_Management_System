0b974414439f3c5e6eed286c8f08c3d0108f9bed
Rukhma <tassadaq187@gmail.com>
2020-06-25 21:26:05 +0500

    Singleton design pattern implemented by Hiabbah Nadeem

---
+++ ProjectSDA/ProjectSDA/Design Patterns/SingletonPattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSDA.Design_Patterns
+{
+    public class SingletonPattern
+    {
+            public static SingletonPattern _obj;
+            public int counter = 0;
+            private SingletonPattern()
+            {
+
+            }
+            public static SingletonPattern getObject()
+            {
+                if (_obj == null)
+                {
+                    _obj = new SingletonPattern();
+                    _obj.counter++;
+                }
+                return _obj;
+            }
+
+            public void Username(string s)
+            {
+                Console.WriteLine(s + " has logged in");
+            }
+
+        }
+
+}
